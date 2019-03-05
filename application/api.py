0ec32e45f518d1025e62831c9f7a6a9333371e4c
ahmed alshammari <ahmed.aalshamari@gmail.com>
2019-03-04 21:58:10 -0500

    Editing api

--- application/api.py
+++ application/api.py
@@ -1,1 +1,1 @@
-print ", ".join(map(lambda n: "Happy Birthday to " + ("you" if n != 2 else "dear {{Name}}"), range(4)))
+print ", ".join(map(lambda n: "Happy Birthday to " + ("you" if n != 2 else "dear {{Ahmed}}"), range(4)))
